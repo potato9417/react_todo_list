552ff2ed7d78718eee06f39ce7afec8ebf55b253
히지니 <potato9417@gmail.com>
2020-06-26 19:18:52 +0900

    수정

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,14 @@ import React from "react";
 class Todo extends React.Component{
     removeTodo=()=>{
         console.log("click")
+        
     }
     render(){
         const {id, todo} = this.props;
         return(
-            <div id="todoList">
-                <p className={id}>{todo}</p>
-                <button className="btns delete" onClick={this.removeTodo}>❌</button>
+            <div className={id}>
+                <p>{todo}</p>
+                <button className="btns delete" onClick={this.removeTodo}>X</button>
             </div>
         )
     }
