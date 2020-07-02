b2eae4c8ca6c7155b24ee2d2efee6b433a1aa65d
히지니 <potato9417@gmail.com>
2020-07-02 11:52:41 +0900

    input 세팅

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,15 +1,26 @@
 import React from "react"
 
-const Add=()=>{
-    return(
-        <section id="addSection">
-            <h3>Add todo</h3>
-            <form>
-                <input type="text" id="addTodoInput"></input>
-            </form>
-            <button className="btns addTodo">ADD!</button>
-        </section>
-    )
+class Add extends React.Component{
+    state={
+        inputTxt:""
+    }
+    
+    setInput=(event)=>{
+        console.log(event.target.value)
+        this.setState({inputTxt:event.target.value})
+    }
+
+    render(){
+        return(
+            <section id="addSection">
+                <h3>Add todo</h3>
+                <form>
+                    <input type="text" id="addTodoInput" value={this.state.inputTxt} onChange={this.setInput}></input>
+                </form>
+                <button className="btns addTodo">ADD!</button>
+            </section>
+        )
+    }
 }
 
 export default Add
