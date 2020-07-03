7874eeab5bffaedc530607bc438dd883ac24fec1
히지니 <potato9417@gmail.com>
2020-07-03 16:13:33 +0900

    음.. 삭제기능..

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,13 @@ import { AiOutlineClose } from "react-icons/ai";
 
 class Todo extends React.Component{
     removeTodo=()=>{
-        console.log("click")
+        console.log(window)
     }
     render(){
         const {id, todo} = this.props;
         return(
-            <div id="todoList">
-                <p className={id}>{todo}</p>
+            <div id="todoList" className={id}>
+                <p >{todo}</p>
                 <button className="btns delete" onClick={this.removeTodo}><AiOutlineClose /></button>
             </div>
         )
