35e9ac50040f87f4c58a42eee531f420ddc37ef8
히지니 <potato9417@gmail.com>
2020-07-03 20:24:57 +0900

    부모태그 찾기 실패 parent가 안먹음..

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,13 +2,13 @@ import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 class Todo extends React.Component{
-    removeTodo=()=>{
-        console.log(window)
+    removeTodo=(event)=>{
+        console.log(event.target.className)
     }
     render(){
         const {id, todo} = this.props;
         return(
-            <div id="todoList" className={id}>
+            <div id="todoList" className={id} onClick={this.removeTodo}>
                 <p >{todo}</p>
                 <button className="btns delete" onClick={this.removeTodo}><AiOutlineClose /></button>
             </div>
